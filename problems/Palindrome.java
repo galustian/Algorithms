143d3aa920ce8d907fa74c110389482a8c703c41
galustian <leonard@galustian.com>
2018-06-14 15:48:33 +0200

    add palindrome problem

---
+++ problems/Palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        var str = args[0];
+        var charArray = str.toCharArray();
+
+        var stack = new Stack<Character>();
+        for (var c: charArray) {
+            stack.push(c);
+        }
+
+        var reverseStr = "";
+        while (stack.size() != 0) {
+            reverseStr += stack.pop();
+        }
+        
+        System.out.println(reverseStr);
+        System.out.println(str);
+        if (reverseStr.equals(str)) {
+            System.out.println("Is a Palindrome!");
+        } else {
+            System.out.println("Not a Palindrome.");
+        }
+    }
+}
