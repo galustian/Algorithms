56804e108442aaf0bdf2dcfb378e50d7d33c5d2f
galustian <leonard@galustian.com>
2018-09-23 08:38:49 +0200

    use pickle instead of deepcopy

--- problems/eight-queens.py
+++ problems/eight-queens.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+import pickle
 
 def check_row(row=0, row_exclude=None): # depth-first search
         if row == 7: return 8 - len(row_exclude[7])
@@ -6,14 +6,14 @@ def check_row(row=0, row_exclude=None): # depth-first search
         for i in range(8):
             if i in row_exclude[row]: continue
 
-            row_exclude_cpy = deepcopy(row_exclude)
+            row_exclude_cpy = pickle.loads(pickle.dumps(row_exclude))
             for row_i in range(1, 8-row): # add row_i exclusions
                 if i+row_i < 8: row_exclude_cpy[row+row_i].add(i+row_i)
                 if i-row_i >= 0: row_exclude_cpy[row+row_i].add(i-row_i)
-                row_exclude_cpy[row+row_i].add(i)        
-            
+                row_exclude_cpy[row+row_i].add(i)
+
             sum_ += check_row(row+1, row_exclude_cpy)
-        
+
         return sum_
 
 if __name__ == '__main__':
